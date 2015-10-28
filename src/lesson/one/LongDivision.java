28d2577b81887f19b50935144fe2d5f444e01a13
Ivan <salivon_ivan@ukr.net>
2015-10-28 22:39:02 +0200

    edit LongDivision

--- src/lesson/one/LongDivision.java
+++ src/lesson/one/LongDivision.java
@@ -5,6 +5,10 @@
  */
 package lesson.one;
 
+import java.util.ArrayList;
+import java.util.Formatter;
+import java.util.Stack;
+
 /**
  *
  * @author Оля
@@ -14,30 +18,74 @@ public class LongDivision {
     private int del1;
     private int del2;
     private double result;
+    private Sta
 
     public LongDivision(int del1, int del2) {
         this.del1 = del1;
         this.del2 = del2;
+    
+    }
+    public static void main(String[] args) {
+        LongDivision ld = new LongDivision(1, 42);
+        ld.calculated();
+        ld.parseResultToArray(ld.getResult());
+        System.out.println(ld.getDel1());
+        for (char arg : ld.parseResultToArray(256)) {
+            System.out.println(arg);
+        }
+         
+    }
+    
+
+    public double getDel1() {
+        return del1;
     }
 
-    public static void main(String[] args) {
-        LongDivision ld = new LongDivision(120, 6);
-        ld.calculated();
+    public double getDel2() {
+        return del2;
     }
 
     void calculated() {
-        Integer del1;
-        Integer del2;
-        int[]arrDel1;
-        del1 = this.del1;
-        del2 = this.del2;
-        String str = del2.toString();
-        for (int i = 0; i < del1.toString().length(); i++) {
-            Math.
-//          int [] ch = str.  
+        printOneString();
+        result = del1 / del2;
+       
+        Formatter format = new Formatter();
+        format.format("%f", result);
+        String s = format.toString();
+
+        System.out.println(result);
+    }
+
+    void int calculated1(int a, int b) {
+        if (a<b) {
+            return;
+        }else{
+           return a-a%b;
         }
+
+    }
+
+    private Stack<Integer> parseResultToStack(int number) {
+        String s = String.valueOf(number);
+        Stack<Integer> result = new Stack<>();
+        for (int i = 0; i <s.length(); i++) {
+           result.push(Integer.valueOf(String.valueOf(s.charAt(i))));
+        }
+        System.out.println(s);
         
-        arrDel1 = new int[del1.]
+        return result;
+
+    }
+
+    private void print() {
+        printOneString();
+        for (int i = 0; i < 10; i++) {
+            
+        }
+    }
+
+    private void printOneString() {
+        System.out.printf("%.1f | %.1f\n", del1, del2);
     }
 
     public void setDel1(int del1) {
