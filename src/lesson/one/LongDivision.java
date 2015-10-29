18fbb276b0918fa4cced10195fcaede247de006e
Ivan <salivon_ivan@ukr.net>
2015-10-29 13:51:07 +0200

    edit LongDivision

--- src/lesson/one/LongDivision.java
+++ src/lesson/one/LongDivision.java
@@ -18,86 +18,97 @@ public class LongDivision {
     private int del1;
     private int del2;
     private double result;
-    private Sta
+    private Stack<Integer> stackDel1;
+    private int count;
 
     public LongDivision(int del1, int del2) {
         this.del1 = del1;
         this.del2 = del2;
-    
+        parseIntToStack(this.del1);
+        countIteration();
+        print();
     }
+
     public static void main(String[] args) {
-        LongDivision ld = new LongDivision(1, 42);
-        ld.calculated();
-        ld.parseResultToArray(ld.getResult());
-        System.out.println(ld.getDel1());
-        for (char arg : ld.parseResultToArray(256)) {
-            System.out.println(arg);
+        LongDivision ld = new LongDivision(25, 24);
+
+    }
+
+    void countIteration() {
+        StringBuilder sb = new StringBuilder(formatResultToStr());
+        while (sb.lastIndexOf("0") != -1) {
+            sb.deleteCharAt(sb.lastIndexOf("0"));
         }
-         
-    }
-    
-
-    public double getDel1() {
-        return del1;
+        count = sb.length() - 2;
     }
 
-    public double getDel2() {
-        return del2;
-    }
-
-    void calculated() {
-        printOneString();
-        result = del1 / del2;
-       
+    private String formatResultToStr() {
+        result = (double) del1 / del2;
         Formatter format = new Formatter();
         format.format("%f", result);
         String s = format.toString();
-
-        System.out.println(result);
+        return s;
     }
 
-    void int calculated1(int a, int b) {
-        if (a<b) {
-            return;
-        }else{
-           return a-a%b;
+    private int calculatedA(int a, int b) {
+        if (a == 0) {
+            return 0;
         }
+        while (a < b && !stackDel1.empty()) {
+            a = combineTwoInt(a, stackDel1.pop());
+        }
+        while (a < b && stackDel1.empty()) {
+            a = combineTwoInt(a, 0);
+        }
+        return a;
 
     }
 
-    private Stack<Integer> parseResultToStack(int number) {
+    private int combineTwoInt(int one, int two) {
+        String oneStr = String.valueOf(one);
+        String twoStr = String.valueOf(two);
+        String combination = oneStr + twoStr;
+        Integer oneTwo = Integer.parseInt(combination);
+        return oneTwo;
+    }
+
+    private void parseIntToStack(int number) {
         String s = String.valueOf(number);
-        Stack<Integer> result = new Stack<>();
-        for (int i = 0; i <s.length(); i++) {
-           result.push(Integer.valueOf(String.valueOf(s.charAt(i))));
+        stackDel1 = new Stack<>();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            stackDel1.push(Integer.valueOf(String.valueOf(s.charAt(i))));
         }
-        System.out.println(s);
-        
-        return result;
-
     }
 
     private void print() {
-        printOneString();
-        for (int i = 0; i < 10; i++) {
-            
+        if (del2 == 0) {
+            printOneString();
+            System.out.println("Division by zero - error!");
+            return;
         }
+        printOneString();
+        String offset = "";
+        int a = calculatedA(stackDel1.pop(), del2);
+        int c = a - a % del2;
+        printTwoString(c, formatResultToStr());
+        System.out.println("----");
+        for (int i = 1; i <= count; i++) {
+            a = a - c;
+            a = calculatedA(a, del2);
+            c = a - a % del2;
+            System.out.println(offset + " " + a);
+            System.out.println(offset + "-" + c);
+            System.out.println(offset + "----");
+            offset += " ";
+        }
+        System.out.println(offset + " " + (a - c));
     }
 
     private void printOneString() {
-        System.out.printf("%.1f | %.1f\n", del1, del2);
+        System.out.printf("  %d | %d\n", del1, del2);
     }
 
-    public void setDel1(int del1) {
-        this.del1 = del1;
+    private void printTwoString(int c, String result) {
+        System.out.println("-" + c + " | " + result);
     }
-
-    public void setDel2(int del2) {
-        this.del2 = del2;
-    }
-
-    public double getResult() {
-        return result;
-    }
-
 }
