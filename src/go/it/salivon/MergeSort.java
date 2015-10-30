ad08b6209156fd41313df40fc133b6d939837728
salivon.i <salivon.i@salivon-iv.txp.com.ua>
2015-10-30 11:56:16 +0200

    Extends Sort edit Mergesort

--- src/go/it/salivon/MergeSort.java
+++ src/go/it/salivon/MergeSort.java
@@ -5,19 +5,23 @@
  */
 package go.it.salivon;
 
+import go.it.main.Sort;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Оля
  */
-public class MergeSort {
+public class MergeSort extends Sort {
 
-    private int[] array;
+    private List<Integer> array;
     private int[] tempMergArr;
     private int length;
 
     public static void main(String a[]) {
 
-        int[] inputArr = {1, 45, 23, 11, 3, 89, 24, 5, 77, 28, 98, 4, 28, 65, 5, 2, 43};
+        List<Integer> inputArr = Arrays.asList(1, 45, 23, 11, 3, 89, 24, 5, 77, 28, 98, 4, 28, 65, 5, 2, 43);
         MergeSort mms = new MergeSort();
         mms.sort(inputArr);
         for (int i : inputArr) {
@@ -26,11 +30,12 @@ public class MergeSort {
         }
     }
 
-    public void sort(int inputArr[]) {
+    public List<Integer> sort(List<Integer> inputArr) {
         this.array = inputArr;
-        this.length = inputArr.length;
+        this.length = inputArr.size();
         this.tempMergArr = new int[length];
         doMergeSort(0, length - 1);
+        return array;
     }
 
     private void doMergeSort(int lowerIndex, int higherIndex) {
@@ -49,23 +54,23 @@ public class MergeSort {
     private void mergeParts(int lowerIndex, int middle, int higherIndex) {
 
         for (int i = lowerIndex; i <= higherIndex; i++) {
-            tempMergArr[i] = array[i];
+            tempMergArr[i] = array.get(i);
         }
         int i = lowerIndex;
         int j = middle + 1;
         int k = lowerIndex;
         while (i <= middle && j <= higherIndex) {
             if (tempMergArr[i] <= tempMergArr[j]) {
-                array[k] = tempMergArr[i];
+                array.set(k, tempMergArr[i]);
                 i++;
             } else {
-                array[k] = tempMergArr[j];
+                array.set(k, tempMergArr[j]);
                 j++;
             }
             k++;
         }
         while (i <= middle) {
-            array[k] = tempMergArr[i];
+            array.set(k, tempMergArr[i]);
             k++;
             i++;
         }
