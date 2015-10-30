bc0a7f19bf825a842043f750e07afa7f60c52ff6
salivon.i <salivon.i@salivon-iv.txp.com.ua>
2015-10-30 13:34:31 +0200

    Extends Sort add ShellSort

--- src/go/it/salivon/ShellSort.java
+++ src/go/it/salivon/ShellSort.java
@@ -5,88 +5,72 @@
  */
 package go.it.salivon;
 
+import go.it.main.Sort;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author Оля
  */
-public class ShellSort {
-   // This class should not be instantiated.
-    private ShellSort() { }
+public class ShellSort extends Sort {
 
-    /**
-     * Rearranges the array in ascending order, using the natural order.
-     * @param a the array to be sorted
-     */
-    public static void sort(Comparable[] a) {
-        int N = a.length;
+    public static void main(String[] args) {
+        ShellSort ss = new ShellSort();
+        ss.show(ss.sort(Arrays.asList(1, 5, 20, 6, 3, 2, 7, 8, 3, 36, 5, 88, 0)));
+    }
 
-        // 3x+1 increment sequence:  1, 4, 13, 40, 121, 364, 1093, ... 
+    public List<Integer> sort(List<Integer> a) {
+        int N = a.size();
         int h = 1;
-        while (h < N/3) h = 3*h + 1; 
+        while (h < N / 3) {
+            h = 3 * h + 1;
+        }
 
         while (h >= 1) {
-            // h-sort the array
             for (int i = h; i < N; i++) {
-                for (int j = i; j >= h && less(a[j], a[j-h]); j -= h) {
-                    exch(a, j, j-h);
+                for (int j = i; j >= h && less(a.get(j), a.get(j - h)); j -= h) {
+                    exch(a, j, j - h);
                 }
             }
-            assert isHsorted(a, h); 
+            assert isHsorted(a, h);
             h /= 3;
         }
         assert isSorted(a);
+        return a;
     }
 
-
-
-   /***************************************************************************
-    *  Helper sorting functions.
-    ***************************************************************************/
-    
-    // is v < w ?
-    private static boolean less(Comparable v, Comparable w) {
+    private boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
-        
-    // exchange a[i] and a[j]
-    private static void exch(Object[] a, int i, int j) {
-        Object swap = a[i];
-        a[i] = a[j];
-        a[j] = swap;
+
+    private void exch(List<Integer> a, int i, int j) {
+        int swap = a.get(i);
+        a.set(i, a.get(j));
+        a.set(j, swap);
     }
 
-
-   /***************************************************************************
-    *  Check if array is sorted - useful for debugging.
-    ***************************************************************************/
-    private static boolean isSorted(Comparable[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (less(a[i], a[i-1])) return false;
+    private boolean isSorted(List<Integer> a) {
+        for (int i = 1; i < a.size(); i++) {
+            if (less(a.get(i), a.get(i - 1))) {
+                return false;
+            }
+        }
         return true;
     }
 
-    // is the array h-sorted?
-    private static boolean isHsorted(Comparable[] a, int h) {
-        for (int i = h; i < a.length; i++)
-            if (less(a[i], a[i-h])) return false;
+    private boolean isHsorted(List<Integer> a, int h) {
+        for (int i = h; i < a.size(); i++) {
+            if (less(a.get(i), a.get(i - h))) {
+                return false;
+            }
+        }
         return true;
     }
 
-    // print array to standard output
-    private static void show(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
-            StdOut.println(a[i]);
+    private void show(List<Integer> a) {
+        for (int i = 0; i < a.size(); i++) {
+            System.out.print(a.get(i) + " ");
         }
     }
-
-    /**
-     * Reads in a sequence of strings from standard input; Shellsorts them; 
-     * and prints them to standard output in ascending order. 
-     */
-    public static void main(String[] args) {
-        String[] a = StdIn.readAllStrings();
-        Shell.sort(a);
-        show(a);
-    }
-
 }
